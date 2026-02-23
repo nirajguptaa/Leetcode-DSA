bd2182a64d56c7a3997e0b481a8716d37b266d51
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-02-23 10:52:10 +0530

    Time: 67 ms (12.13%), Space: 34.5 MB (8.77%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,9 +3,15 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>>res;
         for(string s:strs){
-            string sorted=s;
-            sort(sorted.begin(),sorted.end());
-            res[sorted].push_back(s);
+            vector<int>count(26,0);
+            for(char c:s){
+                count[c-'a']++;
+            }
+            string key="";
+            for(int i=0;i<26;i++){
+                key+="#"+to_string(count[i]);
+            }
+            res[key].push_back(s);
         }
         vector<vector<string>>ans;
         for(auto &pair:res){
