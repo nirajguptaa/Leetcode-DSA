b9266c589a6ad97fced2f6a660b5c67fb79c5e9a
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-03-12 09:52:09 +0530

    Time: 0 ms (100%), Space: 8.5 MB (34.15%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,10 +3,8 @@ public:
     int climbStairs(int n) {
         vector<int>dp(n+1,0);
         if(n<=3)return n;
-        for(int i=0;i<4;i++){
-            dp[i]=i;
-        }
-        for(int i=4;i<=n;i++){
+        dp[0]=1,dp[1]=1;
+        for(int i=2;i<=n;i++){
             dp[i]=dp[i-1]+dp[i-2];
         }
         return dp[n];
