ea5af236cd09e4ed13b0acdd81aa9b7e45434a7b
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-03-14 12:26:12 +0530

    Time: 0 ms (100%), Space: 8 MB (93.35%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,36 +1,12 @@
 class Solution {
-    // string intTObin(int n) {
-    //     string bin="";
-    //     if(n==0)return "0";
-    //     while(n>0){
-    //         bin+=(n%2)+'0';
-    //         n/=2;
-    //     }
-    //     reverse(bin.begin(),bin.end());
-    //     while (bin.size() < 32) {
-    //         bin = "0" + bin;
-    //     }
-    //     return bin;
-    // }
-    // long long binTOint(string num){
-    //     long long ans=0;
-    //     for(int i=0;i<num.size();i++){
-    //         ans=ans*2+(num[i]-'0');
-    //     }
-    //     return ans;
-    // }
 public:
     int reverseBits(int n) {
-        // string bin=intTObin(n);
-        // reverse(bin.begin(),bin.end()); 
-        // int inte=binTOint(bin);
-        // return inte;
-
-        bitset<32>b(n);
-        string s=b.to_string();
-        reverse(s.begin(),s.end());
-        return (int)stoi(s,nullptr,2);
-        
+        int ans=0;
+        for(int i=0;i<32;i++){
+            ans<<=1;
+            ans|=(n&1);
+            n>>=1;
+        }
+        return ans;
     }
-
 };
