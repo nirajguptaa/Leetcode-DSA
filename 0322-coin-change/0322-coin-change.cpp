497dcddbba5e4147c1e4858b79a0f657eef8c2ee
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-03-26 11:51:26 +0530

    Time: 15 ms (92.08%), Space: 17.9 MB (82.43%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -4,10 +4,10 @@ public:
         vector<int>dp(amount+1,1e8);
         dp[0]=0;
         for(int c:coins){
-            for(int i=c;i<=amount;i++){
+            for(int i=c;i<dp.size();i++){
                 dp[i]=min(dp[i],1+dp[i-c]);
             }
         }
-        return dp[amount]>=1e8?-1:dp[amount];
+        return dp[amount]==1e8?-1:dp[amount];
     }
 };
