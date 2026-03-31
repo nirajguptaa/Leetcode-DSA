395eb44fa4974b8309b2587701b0857ee1e82c69
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-03-31 18:10:09 +0530

    Time: 22 ms (51.84%), Space: 50.5 MB (55%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,21 +1,22 @@
 struct Node{
     Node *links[26];
     bool flag=false;
-    bool containKey(char ch){
-        return links[ch-'a']!=NULL;
+    Node *get(char ch){
+        return links[ch-'a'];
     }
     void put(char ch,Node *node){
         links[ch-'a']=node;
     }
-    Node *get(char ch){
-        return links[ch-'a'];
-    }
-   void setEnd(){
+    void setEnd(){
         flag=true;
     }
     bool isEnd(){
         return flag;
     }
+    bool containsKey(char ch){
+        return links[ch-'a']!=NULL;
+    }
+        
 };
 class Trie {
 public:
@@ -27,18 +28,17 @@ public:
     void insert(string word) {
         Node *node=root;
         for(int i=0;i<word.size();i++){
-            if(!node->containKey(word[i])){
+            if(!node->containsKey(word[i])){
                 node->put(word[i],new Node());
             }
             node=node->get(word[i]);
         }
         node->setEnd();
     }
-    
     bool search(string word) {
         Node *node=root;
         for(int i=0;i<word.size();i++){
-            if(!node->containKey(word[i])){
+            if(!node->containsKey(word[i])){
                 return false;
             }
             node=node->get(word[i]);
@@ -49,7 +49,7 @@ public:
     bool startsWith(string prefix) {
         Node *node=root;
         for(int i=0;i<prefix.size();i++){
-            if(!node->containKey(prefix[i])){
+            if(!node->containsKey(prefix[i])){
                 return false;
             }
             node=node->get(prefix[i]);
