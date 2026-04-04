aef0318eddd59bc1fcd937dba2114d8c48149e4d
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-04-04 18:40:52 +0530

    Time: 33 ms (20.74%), Space: 24.5 MB (13.25%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,40 +1,42 @@
 class Solution {
 public:
-    void bfs(int i,int j,vector<vector<char>>& grid,vector<vector<int>>& vis){
+    void bfs(int row,int col,vector<vector<char>>& grid,vector<vector<int>>&vis){
         int n=grid.size();
         int m=grid[0].size();
-        vis[i][j]=true;
+        vis[row][col]=1;
         queue<pair<int,int>>q;
-        q.push({i,j});
+        q.push({row,col});
         while(!q.empty()){
-            int row=q.front().first;
-            int col=q.front().second;
+            int r=q.front().first;
+            int c=q.front().second;
             q.pop();
             int drow[4]={-1, 0, 1, 0};
             int dcol[4]={0, 1, 0, -1};
-            for(int x=0;x<4;x++){
-                int r=row+drow[x];
-                int c=col+dcol[x];
-                if(r>=0 && c>=0 && r<n && c<m && vis[r][c]==0 && grid[r][c]=='1'){
-                    vis[r][c]=1;
-                    q.push({r,c});
+            for(int i=0;i<4;i++){
+                int nr=r+drow[i];
+                int nc=c+dcol[i];
+                if(nr>=0 && nc>=0 && nr<n && nc<m && grid[nr][nc]=='1' && vis[nr][nc]!=1 ){
+                    q.push({nr,nc});
+                    vis[nr][nc]=1;
                 }
             }
+
         }
+
     }
     int numIslands(vector<vector<char>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
+        int count=0;
         vector<vector<int>>vis(n,vector<int>(m,0));
-        int cnt=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(!vis[i][j] && grid[i][j]=='1'){
-                    cnt++;
+                    count++;
                     bfs(i,j,grid,vis);
                 }
             }
         }
-        return cnt;
+        return count;
     }
 };
