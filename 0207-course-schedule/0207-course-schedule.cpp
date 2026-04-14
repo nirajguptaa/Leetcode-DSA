3126788429f3f2816af2b2af7cb1b153a942bb4e
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-04-14 14:01:31 +0530

    Time: 6 ms (50.76%), Space: 20.2 MB (25.31%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,60 +1,34 @@
 class Solution {
 public:
-    //  bool dfs(int node,vector<int>&vis,vector<int>&pathvis,vector<vector<int>>& adj){
-    //     vis[node]=1;
-    //     pathvis[node]=1;
-    //     for(auto it:adj[node]){
-    //         if(!vis[it]){
-    //             if(dfs(it,vis,pathvis,adj)){
-    //                 return true;
-    //             }
-    //         }
-    //         else if(pathvis[it]){
-    //             return true;
-    //         }
-    //     }
-    //     pathvis[node]=0;
-    //     return false;
-    //  }
-            
+    bool dfs(int idx,vector<int>&vis,vector<int>&pathVis,vector<vector<int>>&adj){
+        vis[idx]=1;
+        pathVis[idx]=1;
+        for(auto it:adj[idx]){
+            if(!vis[it]){
+                if(dfs(it,vis,pathVis,adj))return true;
+            }else if(pathVis[it]==true){
+                return true;
+            }
+        }
+        pathVis[idx]=0;
+        return false;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        
-        vector<vector<int>> adj(numCourses);
-        vector<int>indegree(numCourses,0);
-        for(auto it:prerequisites){
-            int u=it[0];
-            int v=it[1];
+        vector<vector<int>>adj(numCourses);
+        for(auto p:prerequisites){
+            int u=p[0];
+            int v=p[1];
             adj[v].push_back(u);
-            indegree[u]++;
         }
-
-        // vector<int>vis(numCourses,0);
-        // vector<int>pathvis(numCourses,0);
-        // for(int i=0;i<numCourses;i++){
-        //     if(!vis[i]){
-        //         if(dfs(i,vis,pathvis,adj)){
-        //             return false;
-        //         }
-        //     }
-        // }
-
-        queue<int>q;
+        //cycle detection
+        vector<int>vis(numCourses,0);
+        vector<int>pathVis(numCourses,0);
         for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0){
-                q.push(i);
+            if(!vis[i]){
+                if(dfs(i,vis,pathVis,adj))return false;
             }
         }
-        int count=0;
-        while(!q.empty()){
-            int top=q.front();
-            q.pop();
-            count++;
-            for(auto it:adj[top]){
-                indegree[it]--;
-                if(indegree[it]==0)q.push(it);
-            }
-        }
-        return count==numCourses;
-            
+        return true;
+
     }
 };
