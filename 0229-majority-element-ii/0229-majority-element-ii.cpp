0edd371d81c9c4a7ef8fbf47330880602fad5516
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-05-12 09:03:16 +0530

    Time: 0 ms (100%), Space: 23.7 MB (63.3%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int>freq;
-        for(auto n:nums){
-            freq[n]++;
+        int ele1=0,ele2=1,cnt1=0,cnt2=0;
+        for(int i=0;i<nums.size();i++){
+            if(ele1==nums[i]){
+                cnt1++;
+            }else if(ele2==nums[i]){
+                cnt2++;
+            }else if(cnt1==0){
+                ele1=nums[i];
+                cnt1=1;
+                
+            }else if(cnt2==0){
+                ele2=nums[i];
+                cnt2=1;
+            }else{
+                cnt1--;
+                cnt2--;
+            }
         }
         int limit=nums.size()/3;
         vector<int>ans;
-        for(auto it:freq){
-            if(it.second>limit){
-                ans.push_back(it.first);
+        int count1=0,count2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==ele1){
+                count1++;
+            }
+            if(nums[i]==ele2){
+                count2++;
             }
         }
+        if(count1>limit)ans.push_back(ele1);
+        if(count2>limit)ans.push_back(ele2);
         return ans;
     }
 };
