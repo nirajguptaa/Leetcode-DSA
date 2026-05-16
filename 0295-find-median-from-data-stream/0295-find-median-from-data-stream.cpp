869b8a8b5e298f8f99164e7ca1a39e24e8d50413
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-05-16 23:05:32 +0530

    Time: 57 ms (88.7%), Space: 148.4 MB (91.19%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -3,29 +3,30 @@ public:
     priority_queue<int>left;
     priority_queue<int,vector<int>,greater<int>>right;
     MedianFinder() {
-        
     }
     
     void addNum(int num) {
-        if(left.size()==0 || left.top()>num){
+        if(left.size()==0 || left.top()>num ){
             left.push(num);
         }else{
             right.push(num);
         }
-        if(left.size()>right.size()+1){
+        if(left.size()-1>right.size()){
             right.push(left.top());
             left.pop();
         }else if(right.size()>left.size()){
             left.push(right.top());
             right.pop();
         }
+        
     }
     
     double findMedian() {
+        
         if(left.size()==right.size()){
-            return (left.top()+right.top())/2.0;
+            return (double)(left.top()+right.top())/2;
         }else{
-            return left.top();
+            return (double)left.top();
         }
     }
 };
