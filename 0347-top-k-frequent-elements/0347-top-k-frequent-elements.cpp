f66f29a5f0f119c71432e7bdbaa39b657ae52df5
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-05-17 00:13:41 +0530

    Time: 5 ms (24.02%), Space: 18 MB (61.84%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -5,15 +5,19 @@ public:
         for(auto n:nums){
             mp[n]++;
         }
-        priority_queue<pair<int,int>>pq;
-        for(auto it:mp){
-            pq.push({it.second,it.first});
-        }
-        vector<int>ans;
-        while(!pq.empty() && k--){
-            ans.push_back(pq.top().second);
+       vector<int>ans;
+       priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+       for(auto it:mp){
+        pq.push({it.second,it.first});
+        if(pq.size()>k){
             pq.pop();
         }
+       }
+       while(k--){
+        ans.push_back(pq.top().second);
+        pq.pop();
+       }
+       
         return ans;
     }
 };
