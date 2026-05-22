ac683f216ec9cd9528fcad7b9e048e784f338a74
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-05-22 10:26:49 +0530

    Time: 6 ms (50.67%), Space: 19.6 MB (42.76%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,34 +1,42 @@
 class Solution {
 public:
-    bool dfs(int idx,vector<int>&vis,vector<int>&pathVis,vector<vector<int>>&adj){
-        vis[idx]=1;
-        pathVis[idx]=1;
-        for(auto it:adj[idx]){
-            if(!vis[it]){
-                if(dfs(it,vis,pathVis,adj))return true;
-            }else if(pathVis[it]==true){
-                return true;
-            }
-        }
-        pathVis[idx]=0;
-        return false;
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>>adj(numCourses);
+        vector<int>indegree(numCourses);
+        vector<int>adj[numCourses];
         for(auto p:prerequisites){
             int u=p[0];
-            int v=p[1];
+            int v=p[1];//v->u
             adj[v].push_back(u);
+            indegree[u]++;
         }
-        //cycle detection
-        vector<int>vis(numCourses,0);
-        vector<int>pathVis(numCourses,0);
+        vector<int>vis(numCourses);
+        queue<int>q;
         for(int i=0;i<numCourses;i++){
-            if(!vis[i]){
-                if(dfs(i,vis,pathVis,adj))return false;
+            if(indegree[i]==0){
+                q.push(i);
             }
         }
-        return true;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            if(!vis[node]){
+                vis[node]=1;
+            }
+            for(auto neigh:adj[node]){
+                indegree[neigh]--;
+                if(indegree[neigh]==0){
+                    q.push(neigh);
+                }
+            }
+        }
+        bool taken=true;
+        for(int i=0;i<numCourses;i++){
+            if(vis[i]!=1){
+                taken=false;
+                break;
+            }
+        }
+        return taken;
 
     }
 };
