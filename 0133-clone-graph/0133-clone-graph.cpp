fe26939d9c4e5a5d90d20f05ac1b7dcccc05ecbe
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-05-22 09:29:27 +0530

    Time: 3 ms (67.97%), Space: 12 MB (79.03%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -21,17 +21,18 @@ public:
 
 class Solution {
 public:
-    unordered_map<Node*, Node*>mp;
+    unordered_map<Node*,Node*>mp;
     Node* cloneGraph(Node* node) {
         if(node==NULL)return NULL;
         if(mp.find(node)!=mp.end()){
             return mp[node];
         }
-        Node *clone =new Node(node->val);
+        Node *clone=new Node(node->val);
         mp[node]=clone;
         for(auto neigh:node->neighbors){
             clone->neighbors.push_back(cloneGraph(neigh));
         }
+
         return clone;
     }
 };
