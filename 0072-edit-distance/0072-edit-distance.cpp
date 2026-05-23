739be817f02ff46ee64a33d7b06e6df4c8bbc44a
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-05-23 14:49:05 +0530

    Time: 11 ms (31.28%), Space: 13.2 MB (39.95%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -12,14 +12,14 @@ public:
         }
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
+
                 if(word1[i-1]==word2[j-1]){
                     dp[i][j]=dp[i-1][j-1];
                 }else{
-                    dp[i][j]=1+min(dp[i-1][j],min(dp[i-1][j-1],dp[i][j-1]));
+                    dp[i][j]=1+min({dp[i-1][j],dp[i-1][j-1],dp[i][j-1]});
                 }
             }
         }
         return dp[n][m];
-        
     }
 };
