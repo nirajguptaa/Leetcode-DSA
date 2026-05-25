121ed5a44db94b63a40608326232b19568768208
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-05-25 07:51:35 +0530

    Time: 27 ms (64.02%), Space: 36.7 MB (47%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int f(int idx,int amount,vector<int>&coins, vector<vector<int>>&dp){
+        if(idx==0){
+            return (amount%coins[idx]==0?1:0);
+        }
+        
+        if(dp[idx][amount]!=-1){
+            return dp[idx][amount];
+        }
+        int notake=f(idx-1,amount,coins,dp);
+        int take=0;
+        if((amount-coins[idx])>=0){
+            take=f(idx,amount-coins[idx],coins,dp);
+        }
+        return dp[idx][amount]=take+notake;
+    }
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        return f(n-1,amount,coins,dp);
+    }
+};
