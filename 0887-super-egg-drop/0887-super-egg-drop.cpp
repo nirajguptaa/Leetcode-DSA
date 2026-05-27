56dafd943c2ed718ae84548abebc59cf513d50d5
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-05-27 20:32:16 +0530

    Time: 59 ms (34.93%), Space: 32.3 MB (36.92%) - LeetHub

--- 0887-super-egg-drop/0887-super-egg-drop.cpp
+++ 0887-super-egg-drop/0887-super-egg-drop.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
-    int solve(int k, int n, vector<vector<int>>& dp) {
-        if (n == 1 || n == 0) {
+    int f(int k,int n,vector<vector<int>>&dp){
+        if(n==0 || n==1){
             return n;
         }
-        if (k == 1) {
+        if(k==1){
             return n;
         }
-        if (dp[k][n] != -1)
+        if(dp[k][n]!=-1){
             return dp[k][n];
-        int mini = INT_MAX;
-
-        // for (int i = 1; i <= n; i++) {
-        //     int temp=1+max(solve(k-1,i-1,dp),solve(k,n-i,dp));
-        //     mini=min(mini,temp);
-        // }
-
+        }
+        int mini=INT_MAX;
         int l=1,h=n;
         while(l<=h){
             int m=l+(h-l)/2;
-            int breakEgg=solve(k-1,m-1,dp);
-            int notBreak=solve(k,n-m,dp);
-            int temp=1+max(breakEgg,notBreak);
+            int breakEgg=f(k-1,m-1,dp);
+            int notbreak=f(k,n-m,dp);
+            int temp=1+max(breakEgg,notbreak);
             mini=min(mini,temp);
-            if(breakEgg>notBreak){
+            if(breakEgg>notbreak){
                 h=m-1;
-
             }else{
                 l=m+1;
             }
+
         }
         return dp[k][n]=mini;
-    };
-    int superEggDrop(int k,int n){
+    }
+    int superEggDrop(int k, int n) {
         vector<vector<int>>dp(k+1,vector<int>(n+1,-1));
-        return solve(k,n,dp);
+        return f(k,n,dp);
     }
 };
