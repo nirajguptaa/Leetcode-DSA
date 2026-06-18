60f6495bd4195450db14146267fa034c15d49dae
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-06-18 14:37:08 +0530

    Time: 0 ms (100%), Space: 10.8 MB (34.89%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int>ans(n+1,0);
-        // for(int i=0;i<n+1;i++){
-        //     ans[i]=__builtin_popcount(i);//O(n log n)
-        //     //For each number i, the number of bits is about:log₂(i)
-        // }
+        
         for(int i=1;i<n+1;i++){
             ans[i]=ans[i>>1]+(i&1);
         }
         return ans;
     }
 };
+//Number of set bits in i = Number of set bits in (i/2) + Last bit of i
