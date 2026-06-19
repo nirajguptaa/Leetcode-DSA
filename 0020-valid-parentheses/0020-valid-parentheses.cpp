181f196459758e3b556cabfdb120ae137589c064
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-06-19 12:00:50 +0530

    Time: 2 ms (15.25%), Space: 8.9 MB (67.15%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;
-        for(char ch:s){
-            if(ch=='(' || ch=='[' || ch=='{'){
+        stack<char> st;
+        for (char ch : s) {
+            if (ch == '(' || ch == '[' || ch == '{') {
                 st.push(ch);
-            }else{
-                if(!st.empty()){
-                    if(ch==')' && st.top()=='('){
-                        st.pop();
-                    }else if(ch=='}' && st.top()=='{'){
-                        st.pop();
-                    }else if(ch==']' && st.top()=='['){
-                        st.pop();
-                    }else{
-                        return false;
-                    }
-                }else{
+            } else {
+                if (st.empty()) {
+                    return false;
+                }
+                if (ch == ')' && st.top() == '(') {
+                    st.pop();
+                } else if (ch == ']' && st.top() == '[') {
+                    st.pop();
+                } else if (ch == '}' && st.top() == '{') {
+                    st.pop();
+                } else {
                     return false;
                 }
             }
         }
         return st.empty();
     }
-
 };
