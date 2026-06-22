f7d0d3b5799de0a8fcb3b967714f2092a82b381e
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-06-22 09:33:31 +0530

    Time: 0 ms (100%), Space: 8.8 MB (84.03%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -5,16 +5,13 @@ public:
         for(char ch:text){
             freq[ch-'a']++;
         }
-        int minSingleFreq=INT_MAX;
-        int minDoubleChar=INT_MAX;
-        minSingleFreq=min(minSingleFreq,freq[0]);
-        minSingleFreq=min(minSingleFreq,freq[1]);
-        minSingleFreq=min(minSingleFreq,freq[13]);
-        minDoubleChar=min(minDoubleChar,freq[11]);
-        minDoubleChar=min(minDoubleChar,freq[14]);
-        minDoubleChar/=2;
-        
-        return min(minSingleFreq,minDoubleChar);
+        return min({
+            freq['b' - 'a'],
+            freq['a' - 'a'],
+            freq['n' - 'a'],
+            freq['l' - 'a'] / 2,
+            freq['o' - 'a'] / 2
+        });
 
     }
 };
