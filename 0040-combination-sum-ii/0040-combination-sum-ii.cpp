436a0fc250f944931203571b1ea3a6d1585a1650
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-06-24 06:25:01 +0530

    Time: 0 ms (100%), Space: 13.9 MB (98.58%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    void f(int currSum,int idx,vector<int>&curr,vector<vector<int>>&ans,vector<int>& candidates, int target){
-        if(currSum==target){
+    void f(int currSum, int idx, vector<int>& curr, vector<vector<int>>& ans,
+           vector<int>& candidates, int target) {
+        if (currSum == target) {
             ans.push_back(curr);
-            return ;
+            return;
         }
-        int n=candidates.size();
-        if(currSum >target || idx==n){
-            return ;
+        int n = candidates.size();
+        if (currSum > target || idx == n) {
+            return;
         }
-        
+
         curr.push_back(candidates[idx]);
-        f(currSum+candidates[idx],idx+1,curr,ans,candidates,target);
+        f(currSum + candidates[idx], idx + 1, curr, ans, candidates, target);
         curr.pop_back();
-        while(idx+1<n && candidates[idx]==candidates[idx+1]){
+        if (currSum + candidates[idx] > target) {
+            return;
+        }
+        while (idx + 1 < n && candidates[idx] == candidates[idx + 1]) {
             idx++;
         }
-        
-        f(currSum,idx+1,curr,ans,candidates,target);
+
+        f(currSum, idx + 1, curr, ans, candidates, target);
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
-        vector<vector<int>>ans;
-        vector<int>curr;
-        f(0,0,curr,ans,candidates,target);
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> ans;
+        vector<int> curr;
+        f(0, 0, curr, ans, candidates, target);
         return ans;
     }
 };
