e5322c9cb7e021c1ddca522e82ea7f95c30ec89d
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-06-28 07:48:02 +0530

    Time: 0 ms (100%), Space: 10.7 MB (58.62%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int f(int s,int e,vector<int>&nums){
-        int n=e-s+1;
-        if(n==0)return 0;
-        if(n==1)return nums[s];
-        vector<int>dp(n,0);
-        if(n==1)return nums[0];
-        dp[0]=nums[s];
-        dp[1]=max(nums[s],nums[s+1]);
-        for(int i=2;i<n;i++){
-            dp[i]=max(nums[s+i]+dp[i-2],dp[i-1]);
+    int f(int idx,int e,vector<int>&nums,vector<int>&dp){
+        if(idx>e)return 0;
+        if(dp[idx]!=-1){
+            return dp[idx];
         }
-        return dp[n-1];
+
+        int rob=nums[idx]+f(idx+2,e,nums,dp);
+        int skip=f(idx+1,e,nums,dp);
+
+        return dp[idx]=max(rob,skip);
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==0)return 0;
         if(n==1)return nums[0];
-        int c1= f(0,n-2,nums);
-        int c2= f(1,n-1,nums);
+        vector<int> dp1(n, -1);
+
+        vector<int> dp2(n, -1);
+        int c1= f(0,n-2,nums,dp1);
+        int c2= f(1,n-1,nums,dp2);
         return max(c1,c2);
     }
 };
