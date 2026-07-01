65a41a00baa4e056a172adbcd591714233cde166
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-07-01 06:57:49 +0530

    Time: 603 ms (28.17%), Space: 637.7 MB (17.64%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -1,53 +1,56 @@
 struct Node{
     Node *links[26];
     bool flag=false;
-    bool containsKey(char ch){
+    bool contains(char ch){
         return links[ch-'a']!=NULL;
     }
-    
+    bool isEnd(){
+        return flag;
+    }
+    void setEnd(){
+        flag=true;
+    }
     void put(char ch,Node *node){
         links[ch-'a']=node;
     }
     Node* get(char ch){
         return links[ch-'a'];
     }
-    void setEnd(){
-        flag=true;
-    }
-    bool isEnd(){
-        return flag;
-    }
 };
 class WordDictionary {
 public:
-    Node *root;
+     Node *root;
     WordDictionary() {
         root=new Node();
     }
+        
     
     void addWord(string word) {
         Node *node=root;
         for(int i=0;i<word.size();i++){
-            if(!node->containsKey(word[i])){
+            if(!node->contains(word[i])){
                 node->put(word[i],new Node());
             }
             node=node->get(word[i]);
         }
         node->setEnd();
     }
-    bool dfs(string &word,Node *node,int ind){
-        if(ind==word.size())return node->isEnd();
-        char ch=word[ind];
+    bool dfs(string word,Node *node,int idx){
+        if(idx==word.size()){
+            return node->isEnd();
+        }
+        char ch=word[idx];
         if(ch!='.'){
-            if(!node->containsKey(ch))return false;
-            return dfs(word,node->get(ch),ind+1);
+            if(!node->contains(ch))return false;
+            return dfs(word,node->get(ch),idx+1);
         }
         for(int i=0;i<26;i++){
             if(node->links[i]!=NULL){
-                if(dfs(word,node->links[i],ind+1)){
+                if(dfs(word,node->links[i],idx+1)){
                     return true;
                 }
             }
+
         }
         return false;
     }
