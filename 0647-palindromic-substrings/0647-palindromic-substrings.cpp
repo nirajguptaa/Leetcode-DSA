32009bfa7a4a7c803e0b066a3951436538dbac49
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-07-17 09:50:23 +0530

    Time: 1103 ms (10.57%), Space: 464.3 MB (5.02%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-    int expand(string &s,int l,int r){
-        int count=0;
-        while(l>=0 && r<s.size() && s[l]==s[r]){
-            count+=1;
-            l--;r++;
-    
+    bool isPalin(string s){
+        int l=0,r=s.size()-1;
+        while(l<r){
+            if(s[l]!=s[r]){
+                return false;
+            }
+            l++;
+            r--;
         }
-        return count;
+        return true;
     }
     int countSubstrings(string s) {
         int count=0;
         for(int i=0;i<s.size();i++){
-            count+=expand(s,i,i);
-            count+=expand(s,i,i+1);
+            string temp="";
+            for(int j=i;j<s.size();j++){
+                temp+=s[j];
+                if(isPalin(temp)){
+                    count++;
+                }
+            }
         }
         return count;
     }
