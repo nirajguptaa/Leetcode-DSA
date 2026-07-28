695702f1d89b20368ff7ec543594d38172fb671e
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-07-28 10:58:12 +0530

    Time: 201 ms (5.05%), Space: 82.1 MB (7.69%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -6,7 +6,7 @@ public:
         string odd="";
         int i=0;
         while(i<s.size()){
-            if(s[i]==s[i+1]){
+            if(i+1<s.size() && s[i]==s[i+1]){
                 first+=s[i];
                 sec+=s[i];
                 i+=2;
