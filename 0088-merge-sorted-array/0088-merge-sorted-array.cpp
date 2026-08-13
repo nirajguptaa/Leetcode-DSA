449299de6ad3155ec7b0a3a4f750a96e9b2fdfeb
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-08-13 21:28:52 +0530

    Time: 0 ms (100%), Space: 12.4 MB (67.3%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -15,12 +15,12 @@ public:
             }
             k--;
         }
+        
         while(j>=0){
             nums1[k]=nums2[j];
-            j--;
             k--;
+            j--;
         }
-       
         
             
     }
