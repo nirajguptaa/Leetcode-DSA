7842b8819068e1028738c063d57662c320d45266
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-08-15 17:53:30 +0530

    Time: 15 ms (92.58%), Space: 17.9 MB (84.31%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int>dp(amount+1,1e8);
+        vector<int>dp(amount+1,1e9);
         dp[0]=0;
         for(int c:coins){
             for(int i=c;i<=amount;i++){
                 dp[i]=min(dp[i],1+dp[i-c]);
             }
         }
-        return dp[amount]==1e8?-1:dp[amount];
-        
+        return dp[amount]==1e9?-1:dp[amount];
     }
 };
