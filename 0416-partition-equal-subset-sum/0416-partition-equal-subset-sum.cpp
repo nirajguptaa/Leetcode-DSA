96a2cb6d7deab31c2512944b62f26571aa800223
Niraj Gupta <162745427+nirajguptaa@users.noreply.github.com>
2026-09-11 11:49:38 +0530

    Time: 91 ms (73.69%), Space: 110.7 MB (23.11%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-        int sum=0;
-        for(int num:nums){
-            sum+=num;
+    bool f(int idx,int curr,int target,vector<int>&nums,vector<vector<int>>&dp){
+        if(curr==target){
+            return true;
         }
-        if(sum%2!=0){
+        if(curr>target || idx>=nums.size()){
             return false;
         }
-        int half=sum/2;
-        // dp[x] is it possible to make sum x
-       vector<bool>dp(half+1,false);
-       dp[0]=true;
-       for(int num:nums){
-        for(int j=half;j>=num;j--){
-            dp[j]=dp[j]||dp[j-num];
+        if(dp[idx][curr]!=-1){
+            return dp[idx][curr];
         }
-       }
-       return dp[half];
-        
+        return dp[idx][curr]=f(idx+1,curr,target,nums,dp)||
+        f(idx+1,curr+nums[idx],target,nums,dp);
+    }
+    bool canPartition(vector<int>& nums) {
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        if(sum%2!=0)return false;
+        int half=sum/2;
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(half+1,-1));
+        return f(0,0,half,nums,dp);
 
-    
     }
 };
